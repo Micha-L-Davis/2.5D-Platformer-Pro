c1e3408438198b6f284f43741242d8ba4e6a4fd0
emeldi <78385236+emeldi@users.noreply.github.com>
2021-05-19 20:55:59 -0700

    code complete, some assembly required

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Elevator.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    private int _status = 0; //0 = not called, 1 = going down, 2 = going up
+    [SerializeField]
+    private List<Transform> _floorList;
+    private int _currentTarget;
+    [SerializeField]
+    private float _speed = 4;
+    private bool _reverse;
+
+
+    private void FixedUpdate()
+    {
+        if (_floorList.Count > 0 && _floorList[_currentTarget] != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _floorList[_currentTarget].position, _speed * Time.deltaTime);
+            float distance = Vector3.Distance(transform.position, _floorList[_currentTarget].position);
+            if (distance == 0f)
+            {
+                StartCoroutine(PauseAtFloor());
+                if (!_reverse)
+                {
+                    _currentTarget++;
+                }
+                else
+                {
+                    _currentTarget--;
+                }
+                if (_currentTarget == _floorList.Count - 1)
+                {
+                    _reverse = true;
+                }
+                else if (_currentTarget == 0)
+                {
+                    _reverse = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+
+    private IEnumerator PauseAtFloor()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+}
